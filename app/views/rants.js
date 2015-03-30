a991a73af8902174963dd9b7e3427970457f6a5d
AndyNhi <nhi.atn@gmail.com>
2015-03-29 22:31:24 -0600

    owner validation

--- app/views/rants.js
+++ app/views/rants.js
@@ -4,11 +4,11 @@ export default Ember.View.extend({
   didInsertElement: function() {
     Ember.$('#thissearch').keyup(function (event) {
       var value = Ember.$('#thissearch').val();
-      if(event.which == 13 && value == ''){
-        Ember.$('#thissearch').attr('placeholder', "This can't be blank!")
+      if(event.which === 13 && value === ''){
+        Ember.$('#thissearch').attr('placeholder', "This can't be blank!");
         event.stopPropagation();
         event.preventDefault();
       }
-    })
+    });
   }
 });
